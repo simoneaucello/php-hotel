44f08a13382e666de20bdc44a64448a67b78d6c8
simoneaucello <simoneaucello@gmail.com>
2024-04-15 16:15:08 +0200

    [add] form

--- index.php
+++ index.php
@@ -40,6 +40,9 @@ $hotels = [
 
 ];
 
+$parcheggio = isset($_GET['parcheggio']) ? $_GET['parcheggio'] : 'all';
+$voto = isset($_GET['voto']) ? $_GET['voto'] : 'all';
+
 
 // foreach ($hotels as $hotel) {
 //   echo "Nome: " . $hotel['name'];
@@ -64,21 +67,49 @@ $hotels = [
 </head>
 
 <body>
-  <div class="container my-5 d-flex justify-content-center flex-wrap">
+  <div class="container my-5">
+    <div class="row">
+      <div class="col-12">
+        <form action="index.php" method="GET">
+          <label for="parcheggio">Filtra per parcheggio:</label>
+          <select class="form-control  mb-3" aria-label="Default select example" id="parcheggio" name="parcheggio">
+            <option value="all">Tutti</option>
+            <option value="true">Si</option>
+            <option value="false">No</option>
+          </select>
 
-    <?php foreach ($hotels as $hotel) : ?>
-      <div class="card m-3" style="width: 18rem;">
-        <ul class="list-group list-group-flush ">
-          <li class="list-group-item"><?php echo "Nome: " . $hotel['name']; ?></li>
-          <li class="list-group-item"><?php echo "Informazioni: " . $hotel['description']; ?></li>
-          <li class="list-group-item"><?php echo $hotel['parking'] ? "Parcheggio: Si" : "Parcheggio: No"; ?></li>
-          <li class="list-group-item"><?php echo "Voto: " . $hotel['vote']; ?></li>
-          <li class="list-group-item"><?php echo "Distanza dal centro: " . $hotel['distance_to_center'] . " Km"; ?></li>
-        </ul>
+          <label for="voto">Filtra per voto:</label>
+          <select class="form-control" aria-label="Default select example" id="voto" name="voto">
+            <option value="all">Tutti</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+          <button type="submit" class="btn btn-primary mt-3">Filtra</button>
+        </form>
       </div>
-    <?php endforeach ?>
 
-  </div>
+
+
+    </div>
+    <div class="container my-5 d-flex justify-content-center flex-wrap">
+
+
+      <?php foreach ($hotels as $hotel) :  ?>
+        <div class="card m-3" style="width: 18rem;">
+          <ul class="list-group list-group-flush ">
+            <li class="list-group-item"><?php echo "Nome: " . $hotel['name']; ?></li>
+            <li class="list-group-item"><?php echo "Informazioni: " . $hotel['description']; ?></li>
+            <li class="list-group-item"><?php echo $hotel['parking'] ? "Parcheggio: Si" : "Parcheggio: No"; ?></li>
+            <li class="list-group-item"><?php echo "Voto: " . $hotel['vote']; ?></li>
+            <li class="list-group-item"><?php echo "Distanza dal centro: " . $hotel['distance_to_center'] . " Km"; ?></li>
+          </ul>
+        </div>
+      <?php endforeach ?>
+
+    </div>
 
 
 </body>
