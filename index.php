de0f236479c12054242386658be2e04b4780840f
simoneaucello <simoneaucello@gmail.com>
2024-04-15 14:47:06 +0200

    [add] print cards

--- index.php
+++ index.php
@@ -40,13 +40,14 @@ $hotels = [
 
 ];
 
-foreach ($hotels as $hotel) {
-  echo "Nome: " . $hotel['name'] . "<br>";
-  echo "Informazioni: " . $hotel['description'] . "<br>";
-  echo "Parcheggio: " . $hotel['parking'] . "<br>";
-  echo "Voto: " . $hotel['vote'] . "<br>";
-  echo "Distanza dal centro: " . $hotel['distance_to_center'] . " Km" . "<br>";
-}
+
+// foreach ($hotels as $hotel) {
+//   echo "Nome: " . $hotel['name'];
+//   echo "Informazioni: " . $hotel['description'];
+//   echo "Parcheggio: " . $hotel['parking'];
+//   echo "Voto: " . $hotel['vote'];
+//   echo "Distanza dal centro: " . $hotel['distance_to_center'] . " Km";
+// }
 
 ?>
 
@@ -56,12 +57,28 @@ foreach ($hotels as $hotel) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.css' integrity='sha512-VcyUgkobcyhqQl74HS1TcTMnLEfdfX6BbjhH8ZBjFU9YTwHwtoRtWSGzhpDVEJqtMlvLM2z3JIixUOu63PNCYQ==' crossorigin='anonymous' />
   <link rel="stylesheet" href="style.css">
 
   <title>PHP Hotel</title>
 </head>
 
 <body>
+  <div class="container my-5 d-flex justify-content-center ">
+
+    <div class="card" style="width: 18rem;">
+      <ul class="list-group list-group-flush">
+        <?php foreach ($hotels as $hotel) : ?>
+          <li class="list-group-item"><?php echo "Nome: " . $hotel['name']; ?></li>
+          <li class="list-group-item"><?php echo "Informazioni: " . $hotel['description']; ?></li>
+          <li class="list-group-item"><?php echo $hotel['parking'] ? "Parcheggio: Si" : "Parcheggio: No"; ?></li>
+          <li class="list-group-item"><?php echo "Voto: " . $hotel['vote']; ?></li>
+          <li class="list-group-item"><?php "Distanza dal centro: " . $hotel['distance_to_center'] . " Km"; ?></li>
+        <?php endforeach ?>
+      </ul>
+    </div>
+
+  </div>
 
 
 </body>
